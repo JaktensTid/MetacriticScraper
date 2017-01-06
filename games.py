f9c12f37f328a6fdf42185c10b297cad4064c914
JaktensTid <jaktenstid1@gmail.com>
2017-01-06 22:22:34 +0300

    Added pages

--- games.py
+++ games.py
@@ -8,7 +8,6 @@ import random
 
 #browser = webdriver.PhantomJS(os.path.join(os.path.dirname(__file__), 'bin/phantomjs'))
 domain = 'http://www.metacritic.com/'
-fh = open('games_pages.csv', 'w')
 proxies = ['http://' + line.replace('\n', '') for line in open('proxies.txt', 'r')]
 SLOW_DOWN = False
 
@@ -23,7 +22,6 @@ def get_html(url):
         html = requests.get(url, headers=headers).content.decode('utf-8')
         if '429 Slow down' in html:
             SLOW_DOWN = True
-            print('SLOW DOWN')
             raise TimeoutError
         return html
     except TimeoutError:
@@ -31,29 +29,31 @@ def get_html(url):
 
 
 def get_pages(genre):
-    writer = csv.writer(fh, delimiter=',',
+    with open('Games/' + genre.split('/')[-2] + '.csv', 'w') as file:
+        writer = csv.writer(file, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    page_content = get_html(genre)
-    document = html.fromstring(page_content)
-    genre_page_sceleton = domain + genre + '&page=%s'
-    try:
-        last_page_number = int(document.xpath("//li[@class='page last_page']/a/text()")[0])
-        pages = [genre] + [genre_page_sceleton % str(i) for i in range(1, last_page_number)]
-        for page in pages:
-            document = html.fromstring(get_html(page))
-            games = [domain + url for url in document.xpath("//ol[@class='list_products list_product_summaries']//h3[@class='product_title']/a/@href")]
-            for game in games:
-                writer.writerow([game])
-                fh.flush()
-                print('url wrote: ' + game)
-    except:
-        print('PAGE CONTENT: ' + page_content.decode('utf-8'))
-        print('GENRE: ' + genre)
-        get_pages(genre)
+        genre_page_sceleton = genre + '&page=%s'
+        def scrape():
+            page_content = get_html(genre)
+            document = html.fromstring(page_content)
+
+            try:
+                last_page_number = int(document.xpath("//li[@class='page last_page']/a/text()")[0])
+                pages = [genre] + [genre_page_sceleton % str(i) for i in range(1, last_page_number)]
+                for page in pages:
+                    document = html.fromstring(get_html(page))
+                    games = [domain + url for url in document.xpath("//ol[@class='list_products list_product_summaries']//h3[@class='product_title']/a/@href")]
+                    print('Page: ' + page + " - - - Games: " + str(len(games)))
+                    for game in games:
+                        writer.writerow([game])
+            except:
+                scrape()
+        scrape()
+
+
 
 if __name__ == "__main__":
     dict = json.load(open('genres.json', 'r'))
     p = Pool(4)
     p.map(get_pages, [dict[key] for key in dict.keys()])
-    fh.close()
     print('Over')
